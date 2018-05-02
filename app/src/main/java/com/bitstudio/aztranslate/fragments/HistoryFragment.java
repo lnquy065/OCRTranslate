1b725f73ea3edbb508cadad0f0dcc21f20fa43d4
qtuancr261 <tuancr261@gmail.com>
2018-05-02 11:11:07 +0700

    Merge From dev-1

--- app/src/main/java/com/bitstudio/aztranslate/fragments/HistoryFragment.java
+++ app/src/main/java/com/bitstudio/aztranslate/fragments/HistoryFragment.java
@@ -23,7 +23,8 @@ import java.util.ArrayList;
 import com.bitstudio.aztranslate.R;
 
 
-public class HistoryFragment extends Fragment {
+public class HistoryFragment extends Fragment
+{
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
--- app/src/main/java/com/bitstudio/aztranslate/fragments/SettingFragment.java
+++ app/src/main/java/com/bitstudio/aztranslate/fragments/SettingFragment.java
@@ -28,7 +28,8 @@ import java.util.ArrayList;
  * Use the {@link SettingFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SettingFragment extends Fragment {
+public class SettingFragment extends Fragment
+{
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
