5971d914e7c1d9bbc632f2b6cc2f067a78896efe
qtuancr261 <tuancr261@gmail.com>
2018-05-03 15:45:56 +0700

    Update color values

---
+++ app/src/main/java/com/bitstudio/aztranslate/Adapter/RecyclerTranslationHistoryTouchHelper.java
@@ -0,0 +1,7 @@
+package com.bitstudio.aztranslate.Adapter;
+
+import android.support.v7.widget.helper.ItemTouchHelper;
+
+public class RecyclerTranslationHistoryTouchHelper extends ItemTouchHelper.SimpleCallback
+{
+}
---
+++ app/src/main/java/com/bitstudio/aztranslate/Adapter/TranslationHistoryAdapter.java
@@ -0,0 +1,7 @@
+package com.bitstudio.aztranslate.Adapter;
+
+import android.support.v7.widget.RecyclerView;
+
+public class TranslationHistoryAdapter extends RecyclerView.Adapter
+{
+}
