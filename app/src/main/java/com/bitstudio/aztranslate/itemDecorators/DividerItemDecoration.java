a6bfc37502b2c40df0817b85da070befaf36e9f9
qtuancr261 <tuancr261@gmail.com>
2018-05-08 16:53:03 +0700

    A simpler Divider Item Decoration - many thanks to Leaking

--- app/src/main/java/com/bitstudio/aztranslate/itemDecorators/DividerItemDecoration.java
+++ app/src/main/java/com/bitstudio/aztranslate/itemDecorators/DividerItemDecoration.java
@@ -1,6 +1,54 @@
 package com.bitstudio.aztranslate.itemDecorators;
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
+import android.view.View;
 
-public class DeviderItemDecoration
-{
-    
+/**
+ * Created by Ravi on 30/10/15.
+ * updated by Thieu on 08/05/2018.
+ */
+public class DividerItemDecoration extends RecyclerView.ItemDecoration {
+
+    private int mColor;
+    private int mHeight; // in px format
+    private Paint paint;
+
+    public DividerItemDecoration(Context context, int color, int height) {
+        this.mColor = color;
+        this.mHeight = height;
+        this.paint = new Paint();
+        paint.setAntiAlias(true);
+    }
+
+    @Override
+    public void onDrawOver(Canvas c, RecyclerView parent) {
+
+        int left = parent.getPaddingLeft();
+        int right = parent.getWidth() - parent.getPaddingRight();
+
+        int childCount = parent.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            View child = parent.getChildAt(i);
+
+            RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child
+                    .getLayoutParams();
+
+            int top = child.getBottom() + params.bottomMargin;
+            int bottom = top + mHeight;
+
+            paint.setColor(mColor);
+            c.drawRect(left, top, right, bottom, paint);
+
+        }
+    }
 }
