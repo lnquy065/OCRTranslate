1c79427df916aa43dd411e7fb27ff2689e368e28
qtuancr261 <tuancr261@gmail.com>
2018-05-09 21:07:19 +0700

    Update TranslationHistoryAdapter 	 - Implements Filterable

--- app/src/main/java/com/bitstudio/aztranslate/adapters/TranslationHistoryAdapter.java
+++ app/src/main/java/com/bitstudio/aztranslate/adapters/TranslationHistoryAdapter.java
@@ -5,6 +5,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -15,10 +17,50 @@ import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 
-public class TranslationHistoryAdapter extends RecyclerView.Adapter<TranslationHistoryAdapter.MyViewHolder>
+public class TranslationHistoryAdapter extends RecyclerView.Adapter<TranslationHistoryAdapter.MyViewHolder> implements Filterable
 {
     private Context context;
     private ArrayList<TranslationHistory> translationHistories;
+    private ArrayList<TranslationHistory> translationHistoriesFiltered;
+
+
+    @Override
+    public Filter getFilter()
+    {
+        return new Filter()
+        {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint)
+            {
+                String charString = constraint.toString();
+                if(charString.isEmpty())
+                {
+                    translationHistoriesFiltered = translationHistories;
+                }
+                else
+                {
+                    ArrayList<TranslationHistory> filteredHis = new ArrayList<>();
+                    for (TranslationHistory rowItem : translationHistories)
+                    {
+                        if (rowItem.getTranslationTime().toLowerCase().contains(charString.toLowerCase()) || rowItem.getTranslationSouceLanguage().toLowerCase().contains(charString.toLowerCase()))
+                            filteredHis.add(rowItem);
+                    }
+                    translationHistoriesFiltered = filteredHis;
+                }
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = translationHistoriesFiltered;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results)
+            {
+                translationHistoriesFiltered = (ArrayList<TranslationHistory>) results.values;
+                notifyDataSetChanged();
+            }
+        };
+    }
+
     public class MyViewHolder extends RecyclerView.ViewHolder
     {
         public TextView textViewScreenshotPath;
@@ -43,6 +85,7 @@ public class TranslationHistoryAdapter extends RecyclerView.Adapter<TranslationH
     {
         this.context = context;
         this.translationHistories = translationHistories;
+        this.translationHistoriesFiltered = translationHistories;
     }
 
     @Override
