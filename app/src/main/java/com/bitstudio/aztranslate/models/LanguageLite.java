43604cd11a380f07455c5150916b1bdf1558374b
LN Quy <lnquy065@gmail.com>
2018-05-17 16:03:30 +0700

    Implements Comparables

--- app/src/main/java/com/bitstudio/aztranslate/models/LanguageLite.java
+++ app/src/main/java/com/bitstudio/aztranslate/models/LanguageLite.java
@@ -1,10 +1,12 @@
 package com.bitstudio.aztranslate.models;
 
+import android.support.annotation.NonNull;
+
 /**
  * Created by LN Quy on 07/05/2018.
  */
 
-public class LanguageLite {
+public class LanguageLite implements Comparable<LanguageLite>{
     public String name;
     public String ocrSymbol;
     public String transSymbol;
@@ -13,5 +15,10 @@ public class LanguageLite {
         this.name = name;
         this.ocrSymbol = ocrSymbol;
         this.transSymbol = transSymbol;
+    }
+
+    @Override
+    public int compareTo(@NonNull LanguageLite languageLite) {
+        return this.name.compareTo(languageLite.name);
     }
 }
